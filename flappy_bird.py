111884dbff8e56eac545df0283f5d49862bc6960
Sergent Ghost <163653913+SergentGhost@users.noreply.github.com>
2024-12-23 22:34:17 +0530

    first commit

---
+++ flappy_bird.py
@@ -0,0 +1,11 @@
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# Screen dimensions
+WIDTH, HEIGHT = 1050, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Flappy Bird")
+
